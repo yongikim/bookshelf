d01dd08a30c8e4508fb92a1f59ac2cf6809ea48f
yongikim <yongi.kim08@gmail.com>
2020-05-18 13:45:11 +0900

    🚀Initial commit

---
+++ apps/web/config/routes.rb
@@ -0,0 +1,7 @@
+# Configure your routes here
+# See: https://guides.hanamirb.org/routing/overview
+#
+# Example:
+# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+root to: 'home#index'
+get '/books', to: 'books#index'
